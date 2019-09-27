d9a912722a726d515205eec499d851e43747df52
vibhoothi <vibhoothiiaanand@gmail.com>
2019-09-27 05:28:56 +0000

    cpu_features: Add AArch64 with NEON cluster
    This patch will be useful for:
    - AArch64 devices which hosts NEON cluster
    - Also helps AArch64 devices compile without NEON cluster if needed using ARM_NATIVE option
    - Add support for CLI input

--- src/cpu_features.rs
+++ src/cpu_features.rs
@@ -17,6 +17,8 @@ pub use native::*;
   any(target_arch = "x86", target_arch = "x86_64")
 ))]
 pub use x86::*;
+#[cfg(any(target_arch = "aarch64", target_arch = "aarch64"))]
+pub use aarch64::*;
 
 #[cfg(all(
   feature = "nasm",
@@ -94,4 +96,53 @@ mod native {
       CpuFeatureLevel::NATIVE
     }
   }
+}
+
+#[cfg(any(target_arch = "aarch64", target_arch = "aarch64"))]
+mod aarch64 {
+
+    use arg_enum_proc_macro::ArgEnum;
+    use std::env;
+
+    #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, ArgEnum)]
+    pub enum CpuFeatureLevel{
+        ARM_NATIVE,
+        NEON,
+    }
+
+    impl CpuFeatureLevel {
+        pub const fn len() -> usize {
+            CpuFeatureLevel::NEON as usize + 1
+        }
+
+    #[inline(always)]
+    pub fn as_index(self) -> usize {
+        const LEN: usize = CpuFeatureLevel::len();
+        assert_eq!(LEN & (LEN -1), 0);
+        self as usize & (LEN -1)
+      }
+    }
+
+    impl Default for CpuFeatureLevel {
+        fn default() -> CpuFeatureLevel {
+            let detected: CpuFeatureLevel = if is_aarch64_feature_detected!("neon") {
+                CpuFeatureLevel::NEON
+        } else {
+            CpuFeatureLevel::ARM_NATIVE
+        }
+        };
+        let manual: CpuFeatureLevel = match env::var("RAV1E_CPU_TARGET") {
+            Ok(feature) => match feature.as_ref() {
+                "rust" => CpuFeatureLevel::ARM_NATIVE,
+                "neon" => CpuFeatureLevel::NEON,
+                _ => detected,
+            },
+            Err(_e) => detected,
+        };
+        if manual > detected {
+            detected
+        } else {
+            manual
+        }
+    }
 }

7806aec787e3bcc2395593b868a6f51277b8a7ce
vibhoothi <vibhoothiiaanand@gmail.com>
2019-10-15 15:29:06 +0000

    build: Add rav1e_ prefix to the functions

--- build.rs
+++ build.rs
@@ -63,8 +63,8 @@ fn build_nasm_files() {
 fn build_asm_files() {
   cc::Build::new().file("src/arm/64/add.s").compile("my-asm-lib");
   cc::Build::new().file("src/arm/asm.S").compile("asm-s");
-  cc::Build::new().file("src/arm/64/util.S").include(".").compile("util-s");
-  cc::Build::new().file("src/arm/64/mc.S").include(".").compile("mc-s");
+  cc::Build::new().file("src/arm/64/util.S").include(".").define("PRIVATE_PREFIX", "rav1e_").compile("util-s");
+  cc::Build::new().file("src/arm/64/mc.S").include(".").define("PRIVATE_PREFIX", "rav1e_").compile("mc-s");
 }
 
 fn rustc_version_check() {

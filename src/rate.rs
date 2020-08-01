8740c2acd6e00d3a2ec83d825b3ed2e3045ef3ea
Vibhoothi <vibhoothiiaanand@gmail.com>
2020-08-01 23:01:40 +0530

    rate: s1 1.00 s2 2.25 gm lat

--- src/rate.rs
+++ src/rate.rs
@@ -70,15 +70,15 @@ const MQP_Q12: &[i32; FRAME_NSUBTYPES] = &[
 
 // To-do: Add documentaiton after debugging
 // DQP_Q57[i] = log_2(sqrt(w_1/w_i))*(1 << 57)
-// 0                       7.443935 [Negetive]
-// 1                       4.638141 [Zero]
+// 0                       5.008725 [Negetive]
+// 1                       3.259733 [Zero]
 // 2                       2.085997 [Positve]
 // 3                       1.302035 [Postive]
 
 pub fn calc_dqp_q57(temp_dqp57: usize) -> i64 {
   const MEAN_DW: &[f64; FRAME_NSUBTYPES] = &[
-    ( 7.443935 as f64),
-    ( 4.638141 as f64),
+    ( 5.008725 as f64),
+    ( 3.259733 as f64),
     ( 2.085997 as f64),
     ( 1.302035 as f64),
   ];

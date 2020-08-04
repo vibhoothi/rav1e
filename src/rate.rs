298408e45257424f27486b8b2d9d6b569d702911
Vibhoothi <vibhoothiiaanand@gmail.com>
2020-08-04 21:16:41 +0530

    rate: s1 1.00 s2 4.00 am

--- src/rate.rs
+++ src/rate.rs
@@ -70,10 +70,10 @@ const MQP_Q12: &[i32; FRAME_NSUBTYPES] = &[
 
 pub fn calc_dqp_q57(temp_dqp57: usize) -> i64 {
   const MEAN_DW: &[f64; FRAME_NSUBTYPES] = &[
-    (33.122698 as f64),
-    (13.930213 as f64),
-    ( 2.751793 as f64),
-    ( 1.437196 as f64),
+    (51.807453 as f64),
+    (22.090553 as f64),
+    ( 3.026701 as f64),
+    ( 1.484094 as f64),
   ];
   // To-do: Return directly after debugging
   let dummy = ((MEAN_DW[1] / MEAN_DW[temp_dqp57]).sqrt().log2()
